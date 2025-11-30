b2594f40587f59ceabc04320f11df745f1ae5dd2
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-11-30 20:11:38 +0300

    feat(backend): add profile and admin stats endpoints

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Controller, UseGuards, Get, Req } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AdminGuard } from 'src/roles/admin.guard';
+import { type Request } from 'express';
 
-@Controller('users')
+@Controller('')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
+    return {
+      user: req.user,
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @UseGuards(AuthGuard, AdminGuard)
+  @Get('admin/stats')
+  getAdminStats() {
+    return {
+      totalUsers: 105,
+      revenue: '$50,000',
+      activeUsers: 12,
+    };
   }
 }
