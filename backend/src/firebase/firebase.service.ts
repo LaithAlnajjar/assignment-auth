bac456fdf3cac31a81c51bc94cb30bc60e7f250c
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-11-30 18:18:30 +0300

    feat(backend): create auth and firestore instances in repository

--- backend/src/firebase/firebase.service.ts
+++ backend/src/firebase/firebase.service.ts
@@ -1,12 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 import { app } from 'firebase-admin';
 
 @Injectable()
 export class FirebaseRepository {
-  // eslint-disable-next-line no-unused-private-class-members
   #db: FirebaseFirestore.Firestore;
+  #auth: admin.auth.Auth;
 
   constructor(@Inject('FIREBASE_APP') private firebaseApp: app.App) {
     this.#db = firebaseApp.firestore();
+    this.#auth = firebaseApp.auth();
+  }
+
+  get firestore() {
+    return this.#db;
+  }
+
+  get auth() {
+    return this.#auth;
   }
 }
