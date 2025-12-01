889e3b45415d37d794937e5592dbe39602f7f07a
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-12-01 19:53:10 +0300

    feat(auth): update axios instace to intercept 401 errors and attempt to refresh token

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -4,4 +4,33 @@ const baseURL = "http://localhost:3000/api";
 
 export const api = axios.create({
   baseURL,
+  withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await api.post("/auth/refresh");
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh failed. User session is invalid.", refreshError);
+
+        if (typeof window !== "undefined") {
+          window.location.href = "/login";
+        }
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
